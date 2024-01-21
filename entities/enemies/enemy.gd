f2ab7bdbb93865bff9ff25b962e2b9d081a911af
Eduardo Mello <eedu.mello@gmail.com>
2024-01-21 11:47:06 -0600

    Add collision avoidance

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -20,17 +20,22 @@ func _ready() -> void:
 
 	var objective: Node2D = $/root/Map/Objective
 	nav_agent.set_target_position(objective.global_position)
+	nav_agent.max_speed = speed
 
 
 func _move(delta: float) -> void:
 	var next_path_pos: Vector2 = nav_agent.get_next_path_position()
 	var cur_agent_pos: Vector2 = global_position
 	var new_velocity: Vector2 = cur_agent_pos.direction_to(next_path_pos) * speed
-	velocity = new_velocity
+	if not nav_agent.avoidance_enabled:
+		velocity = new_velocity
+		move_and_slide()
+	else:
+		nav_agent.set_velocity(new_velocity)
 	
 	animated_sprite_2d.global_rotation = _calculate_rot(animated_sprite_2d.global_rotation, velocity.angle(), rot_speed, delta )
 	collision_shape_2d.global_rotation = _calculate_rot(collision_shape_2d.global_rotation, velocity.angle(), rot_speed, delta )
-	move_and_slide()
+	
 
 
 func set_health(value: int) -> void:
@@ -44,6 +49,7 @@ func die() -> void:
 	speed = 0
 	animated_sprite_2d.play("die")
 	default_sound.stop()
+	nav_agent.set_velocity(Vector2.ZERO)
 	
 	
 func play_animation(anim_name: String) -> void:
@@ -61,3 +67,8 @@ func _calculate_rot(start_rot: float, target_rot: float, _speed: float, delta: f
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animated_sprite_2d.animation == "die":
 		queue_free()
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+	move_and_slide()
